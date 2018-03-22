e76e69ad507a61ed879ff2e0d0c4e723f32d268c
Quentin Santos <quentin.santos@worldonline.fr>
2018-03-22 07:59:19 +0100

    Random negate protocol for (mock) shared Paillier

--- mpcprotocols.py
+++ mpcprotocols.py
@@ -103,3 +103,41 @@ class SharedMockMPCProtocols(MockMPCProtocols):
             paillier.PaillierPublicKeyShare.assemble_decryption_shares(self.pk_shares, decryption_shares)
             for decryption_shares in decryption_shares_batch
         ]
+
+    def random_negate_batched(self, x_batch, y_batch):
+        """Random negate protocol
+
+        Arguments:
+            x_batch (list): the left values (paillier.PaillierCiphertext) to randomly negate
+            y_batch (list): the right values (paillier.PaillierCiphertext) to randomly negate
+
+        Returns:
+            tuple: a pair of two lists, x_batch and y_batch, where each (x, y)
+            pair (from `zip(x_batch, y_batch)`) is two Paillier ciphertexts
+            encrypting either the same values as the corresponding inputs, or
+            their negation
+        """
+        pk = self.pk_shares[0].public_key
+
+        for _ in self.pk_shares:
+            r_batch = [random.choice([-1, 1]) for _ in x_batch]
+
+            rx_batch = []
+            for r, x in zip(r_batch, x_batch):
+                _, x = util.run_protocol(
+                    pk.prove_private_multiply(r, x),
+                    pk.verify_private_multiply(x),
+                )
+                rx_batch.append(x)
+
+            ry_batch = []
+            for r, y in zip(r_batch, y_batch):
+                _, y = util.run_protocol(
+                    pk.prove_private_multiply(r, y),
+                    pk.verify_private_multiply(y),
+                )
+                ry_batch.append(y)
+
+            x_batch, y_batch = rx_batch, ry_batch
+
+        return x_batch, y_batch
